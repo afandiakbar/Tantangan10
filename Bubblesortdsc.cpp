f1073f487f398bd23366f62af683baf6b03cbfe0
afandiakbar <49123055+afandiakbar@users.noreply.github.com>
2019-04-08 20:52:05 +0700

    Update Bubblesortdsc.cpp

--- Bubblesortdsc.cpp
+++ Bubblesortdsc.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 void swap(int *xp, int *yp)
 {
     int temp = *xp;
@@ -8,15 +7,13 @@ void swap(int *xp, int *yp)
     *yp = temp;
 }
 
-
 void bubbleSort (int arr[], int n)
 {
     int i, j;
-    for (i = 0; i < n-1; i++)
+    for (i = 7-1; i>=0; i--)
     
-    
-       for (j = 0; j < n-i-1; j++)
-           if (arr[j] > arr[j+1])
+       for ( j=0; j<=n-1; j++)
+           if (arr[j] < arr[j+1])
               swap (&arr[j], &arr[j+1]);
 }
 void printArray (int arr[], int size)
@@ -26,8 +23,6 @@ void printArray (int arr[], int size)
         printf ("%d ", arr[i]);
     printf("n");
 }
-
-
 int main()
 {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
